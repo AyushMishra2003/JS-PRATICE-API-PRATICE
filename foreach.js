1240b54f5c913349bb27dd1e5e08e985656f13f0
unknown <ayushm185@gmail.com>
2023-11-01 18:13:45 +0530

    loop problem solved

---
+++ foreach.js
@@ -0,0 +1,4 @@
+const coding=[1,2,3,4,5]
+coding.forEach( function (item){
+    console.log(item);
+})
--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const  parent=document.querySelector('#img')
-console.log(img);
 
 
 fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=0&limit=20').then(function(resonse){
     return resonse.json()
 })
 .then(function(resp1){
+    resp1.photos.forEach(function(item){
+         const child=document.createElement('div')
+         child.id="child"
+         const image=document.createElement('IMG')
+         image.src=item.url
+         child.appendChild(image)
+         parent.appendChild(child)
+         const para=document.createElement('p')
+         para.innerHTML=item.description;  
+         child.appendChild(para)
+         parent.appendChild(child)
+    })
+    /*
     for(let i=0;i<=19;i++)
     { 
-        const child=document.createElement('div')
-        child.id="child"
+         const child=document.createElement('div')
+         child.id="child"
          const image=document.createElement('IMG')
          image.src=resp1.photos[i].url 
          child.appendChild(image)
@@ -19,7 +31,7 @@ fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=0&limit=20').th
          child.appendChild(para)
          parent.appendChild(child)
     }
-
+     */
 })
 .catch(function(error){
    console.log(error);
